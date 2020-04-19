c8dedb3381e69ccab1bdc9c0d09e2f643d9a57a9
Aniket <aniketupatel1@gmail.com>
2020-04-19 19:03:15 -0400

    added first title heading with animation

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -36,10 +36,43 @@ import Animate from 'react-move/Animate'
         </Animate>
     )
 
+
+    animateFirst = () => (
+        <Animate
+            show={true}
+            start={{
+                opacity: 0,
+                x: 503,
+                y: 450
+            }}
+            enter={{
+                opacity:[1],
+                x: [273],
+                y: [450],
+                timing:{duration:500, ease: easePolyOut}
+            }}
+        >
+            {({opacity, x, y}) => {
+                return (
+                    <div 
+                    className='featured_first'
+                    style={{
+                        opacity,
+                        transform: `translate(${x}px, ${y}px)`
+                    }}
+                    >
+                        League
+                    </div>
+                )
+            }}
+        </Animate>
+    )
+
     render() {
         return (
             <div className='featured_text'>
                 {this.animateNumber()}
+                {this.animateFirst()}
             </div>
         )
     }
