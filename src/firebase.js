d406a5dc9c9ddfdd6cd2e84b243c59383fd50fc7
Aniket <aniketupatel1@gmail.com>
2020-04-21 15:31:38 -0400

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMxrxi8lyNoFQp9Xa0oHDDIfSApCjLxTs",
+    authDomain: "m-city-84f99.firebaseapp.com",
+    databaseURL: "https://m-city-84f99.firebaseio.com",
+    projectId: "m-city-84f99",
+    storageBucket: "m-city-84f99.appspot.com",
+    messagingSenderId: "93959713345",
+    appId: "1:93959713345:web:4c2eaa14d921821048ccc0",
+    measurementId: "G-NLFKFMG4V5"
+};
+
+
+firebase.initializeApp(firebaseConfig)
