71ccc7d1a734cad62dc3b6e5c7c8e894ecf12f0a
Aniket <aniketupatel1@gmail.com>
2020-04-28 16:42:21 -0400

    added to validate function to check for correct email format with regex and to show messagea

--- src/Components/ui/misc.js
+++ src/Components/ui/misc.js
@@ -52,6 +52,11 @@ export const reverseArray = (actualyArray) => {
 export const validate = (element) => {
     let error = [true, '']
 
+    if(element.validation.email){
+        const valid = /\S+@\S+\.\S+/.test(element.value) // regex to check email 
+        const message = `${!valid ? 'This must be a valid email' : ''}`
+        error = !valid ? [valid, message] : error
+    }
 
     if(element.validation.required) {
         const valid = element.value.trim() !== ''
