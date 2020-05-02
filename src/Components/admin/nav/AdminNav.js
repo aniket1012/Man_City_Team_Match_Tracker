b650c2f442b7cd9314339b96aff5352411e1f69b
Aniket <aniketupatel1@gmail.com>
2020-05-02 17:37:26 -0400

    creating nav bar with links and routes to add player and matches

---
+++ src/Components/admin/nav/AdminNav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import ListItem from '@material-ui/core/ListItem'
+
+const AdminNav = () => {
+
+    const links = [
+        {
+            title: 'Matches',
+            linkTo: '/admin_matches'
+        },
+        {
+            title: 'Add Match',
+            linkTo: '/admin_matches/edit_match'
+        },
+        {
+            title: 'Players',
+            linkTo: '/admin_players'
+        },
+        {
+            title: 'Add Player',
+            linkTo: '/admin_players/add_player'
+        },
+        
+    ]
+
+
+    const renderItems = () => {
+        links.map((link) => (
+            <Link to={link.linkTo} key={link.title}>
+            
+            </Link>
+        ))
+    }
+
+
+    return (
+        <div>
+            {renderItems()}
+        </div>
+    );
+};
+
+export default AdminNav;
--- src/HOC/AdminLayout.js
+++ src/HOC/AdminLayout.js
@@ -4,7 +4,7 @@ const AdminLayout = (props) => {
     return (
         <div className='admin_container'>
             <div className='admin_left_nav'>
-
+                nav
             </div>
             <div className='admin_right'>
                 {props.children}
