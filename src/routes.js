0232903b366e15c98f6377fee46a9f54e3d4a040
Aniket <aniketupatel1@gmail.com>
2020-05-02 17:29:35 -0400

    started creating admin feature

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Layout from './HOC/Layout'
 // import { Switch } from '@material-ui/core';
 import {Switch, Route} from 'react-router-dom'
-import SignIn from './Components/signin'
 
+import SignIn from './Components/signin'
 import Home from './Components/home'
 
+import Dashboard from './Components/admin/Dashboard'
 
 const Routes = (props) => {
 
@@ -14,6 +15,7 @@ const Routes = (props) => {
     <div>
       <Layout>
         <Switch>
+          <Route exact component={Dashboard} path='/dashboard'/>
           <Route exact component={SignIn} path='/sign_in'/>
           <Route exact component={Home} path='/'/>
         </Switch>
