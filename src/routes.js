91534466b38208ecfc6dfcb44a370ac23a225b91
Aniket <aniketupatel1@gmail.com>
2020-05-05 18:55:52 -0400

    added form field to edit match route, created state to monitor form field for event, and adjusted formfield to include label check

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import SignIn from './Components/signin'
 
 import Dashboard from './Components/admin/Dashboard'
 import AdminMatches from './Components/admin/matches'
+import AddEditMatch from './Components/admin/matches/addEditMatch'
 
 
 const Routes = (props) => {
@@ -21,6 +22,7 @@ const Routes = (props) => {
       <Layout>
         <Switch>
 
+          <PrivateRoute {...props} path='/admin_matches/edit_match/:id' exact component={AddEditMatch}/>
           <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/>
           <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
           <PublicRoute {...props} restricted={true} path='/sign_in' exact component={SignIn}/>
