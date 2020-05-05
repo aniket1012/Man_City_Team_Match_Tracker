938eb6e23899c91d3ab1384b97d6b1e24e9d2135
Aniket <aniketupatel1@gmail.com>
2020-05-05 18:26:44 -0400

    created Table for matches and imported firebase data and added styling to final

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import Home from './Components/home'
 import SignIn from './Components/signin'
 
 import Dashboard from './Components/admin/Dashboard'
+import AdminMatches from './Components/admin/matches'
 
 
 const Routes = (props) => {
@@ -20,6 +21,7 @@ const Routes = (props) => {
       <Layout>
         <Switch>
 
+          <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/>
           <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
           <PublicRoute {...props} restricted={true} path='/sign_in' exact component={SignIn}/>
           <PublicRoute {...props} restricted={false} path='/' exact component={Home}/>
