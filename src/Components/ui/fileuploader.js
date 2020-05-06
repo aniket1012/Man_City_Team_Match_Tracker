d1e81c56dacfcc7906c0e0daa3f8e03997b450da
Aniket <aniketupatel1@gmail.com>
2020-05-06 15:10:02 -0400

    added function in state to replace state with information derived from props recieved by addEditPlayers comopnent

--- src/Components/ui/fileuploader.js
+++ src/Components/ui/fileuploader.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import { firebase } from '../../firebase'
+import FileUploader from 'react-firebase-file-uploader'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 class Fileuploader extends Component {
+
+
+    state = {
+        name: '',
+        isUploading: false,
+        fileURL: '',
+
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if(props.defaultImg) {
+            return state = {
+                name: props.defaultImgName,
+                fileURL: props.defaultImg 
+            }
+        }
+        return null
+    }
+
     render() {
         return (
             <div>
