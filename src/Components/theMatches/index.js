b3b57393cc78cab6de194775e54789e2c2c51673
Aniket <aniketupatel1@gmail.com>
2020-05-08 15:51:45 -0400

    created component did mount function on index of theMatches to pull data from firebase server

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+import { firebaseMatches } from '../../firebase'
+import {firebaseLooper, reverseArray } from '../ui/misc'
+
 
 class TheMatches extends Component {
+
+    state = {
+        loading: true,
+        matches: [],
+        filteredMatches: [],
+        playerFilter: 'All',
+        resultFilter: 'All'
+    }
+
+    componentDidMount() {
+        firebaseMatches.once('value').then((snapshot)=> {
+            const matches = firebaseLooper(snapshot)
+
+            this.setState({
+                loading: false,
+                matches: reverseArray(matches),
+                filteredMatches: reverseArray(matches)
+            })
+        })
+    }
+
     render() {
+
+        console.log(this.state);
+        
         return (
             <div>
                 the TheMatches
