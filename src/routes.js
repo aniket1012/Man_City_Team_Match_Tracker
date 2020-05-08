2e8466317b53827820788b255bee3257f81da0e9
Aniket <aniketupatel1@gmail.com>
2020-05-08 15:44:41 -0400

    created match dir with index and list componenet along with public route

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import PublicRoute from './Components/authRoutes/publicRoutes'
 import Home from './Components/home'
 import SignIn from './Components/signin'
 import TheTeam from './Components/theTeam'
+import TheMatches from './Components/theMatches/index'
 
 import Dashboard from './Components/admin/Dashboard'
 import AdminMatches from './Components/admin/matches'
@@ -33,7 +34,9 @@ const Routes = (props) => {
           <PrivateRoute {...props} path='/admin_matches/edit_match/:id' exact component={AddEditMatch}/>
           <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/>
           <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
+
           <PublicRoute {...props} restricted={true} path='/sign_in' exact component={SignIn}/>
+          <PublicRoute {...props} restricted={false} path='/the_matches' exact component={TheMatches}/>
           <PublicRoute {...props} restricted={false} path='/the_team' exact component={TheTeam}/>
           <PublicRoute {...props} restricted={false} path='/' exact component={Home}/>
          
